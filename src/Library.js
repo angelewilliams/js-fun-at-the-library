0d4b051d9c6fdc5ca9ac2c66ee1ca3723296b25e
Angele Williams <angele.williams5@gmail.com>
2022-02-06 19:28:25 -0700

    Complete Library tests

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,67 @@
+//createLibrary should take in a parameter
+function createLibrary(libraryName) {
+   // should outputt an object with a key '.name'
+  var createdLibrary = {
+    name: libraryName,
+    //shelves is an object, with properties that hold arrays (eventually to hold books)
+    shelves: {
+      //each shelf is labeled with a genre, and is currently an empty array
+      fantasy:[],
+      fiction:[],
+      nonFiction:[]
+    },
+  };
+  return createdLibrary
+}
+//after trying to refactor this I was trying to make the bookToAdd.name
+//into a comparable data type to the object, but it was a mess and
+//ultimately my solution below was 'cleaner' even if its a lot of lines
+function addBook(createdLibrary, bookToAdd) {
+  //we need to have the createdLibrary object (1st param) to
+  //be able to add the book to a shelf (empty array)
+  //need to be able to identify the genre, stored in the bookToAdd parameter (since will be passing through a book object)
+  if (bookToAdd.genre === "fantasy") {
+    createdLibrary.shelves.fantasy.push(bookToAdd);
+  }
+  else if (bookToAdd.genre === "fiction") {
+    createdLibrary.shelves.fiction.push(bookToAdd);
+  }
+  else if (bookToAdd.genre === "nonFiction") {
+    createdLibrary.shelves.nonFiction.push(bookToAdd);
+  }
+  else (console.log("Unable to locate correct shelf."))
+  // createdLibrary.shelves.nonFiction.push(bookToAdd);
+  return
+}
 
+function checkoutBook(createdLibrary, bookTitle, bookGenre) {
+//if the bookTitle param is equal to the title, stored in the createdLibrary object
+//we want to see if it is there and to remove it from the shelf to check it out
+
+//created confirm and error to simplify the return
+  var confirm = `You have now checked out ${bookTitle} from the ${createdLibrary.name}`;
+  var error = `Sorry, there are currently no copies of ${bookTitle} available at the ${createdLibrary.name}`;
+//iterate through library, shelves, bookGenre to check is bookTitle === title
+
+// what do I need to do an if statement that includes if genre array is empty
+  //started if statement to see if genre array was empty, if so book cannot be checked out, return error
+   if (createdLibrary.shelves[bookGenre].length === 0) {
+    return error;
+    }
+    //if the bookTitle is === to the title, meaning it is in the library and can check it out
+    else if (bookTitle === createdLibrary.shelves[bookGenre][0].title) {
+  //access createdLibrary obj, shelves and then the genre from the parameter bookGenre
+  //then I want to remove that book object from the createdLibrary object
+      createdLibrary.shelves[bookGenre].splice(0, 1);
+        return confirm
+    }
+  }
+//UNUSED CODE
+//Originally tried to use: for (var i = 0; i < createdLibrary.shelves[bookGenre].length; i++) {
+//else if (bookTitle != createdLibrary.shelves[bookGenre][i]) {
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
--- test/Library-test.js
+++ test/Library-test.js
@@ -8,29 +8,31 @@ var {
 
 describe("library.js", function() {
   describe("createLibrary", function() {
-    it.skip("should be a function", function() {
+    it("should be a function", function() {
       assert.isFunction(createLibrary);
     });
 
-    it.skip("should have a name", function() {
+    it("should have a name", function() {
+      // createLibrary takes in a parameter that is a string
       var denverLibrary = createLibrary("Denver Public Library");
-
+//when createLibrary takes in string, the object denverLibrary should
+//have a key of 'name' that the parameter goes into
     assert.equal(denverLibrary.name, "Denver Public Library");
     });
 
-    it.skip("should be able to have a different name", function() {
+    it("should be able to have a different name", function() {
       var goldenLibrary = createLibrary("Golden Public Library");
 
       assert.equal(goldenLibrary.name, "Golden Public Library");
     });
 
-    it.skip("should have shelves", function() {
+    it("should have shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
 
       assert.isObject(denverLibrary.shelves);
     });
 
-    it.skip("should have several different types of shelves", function() {
+    it("should have several different types of shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
 
       assert.deepEqual(denverLibrary.shelves.fantasy, []);
@@ -40,21 +42,25 @@ describe("library.js", function() {
   });
 
   describe("addBook", function() {
-    it.skip("should add book to the fantasy shelf", function() {
+    it("should add book to the fantasy shelf", function() {
       var denverLibrary = createLibrary("Denver Public Library");
+//dracula is an object with 4 key value pairs
       var dracula = {
         title: "Dracula",
         mainCharacter: { name: "Count Dracula", age: undefined, pronouns: "he/him" },
         pageCount: 418,
         genre: "fantasy"
       }
-
+//addBook should take in two parameters (both are objects)
       addBook(denverLibrary, dracula);
-
+//looks like we need the first parameter to identify the library and then add
+//the second paremeter to push the second parameter (the book) onto a specific shelf
       assert.equal(denverLibrary.shelves.fantasy[0], dracula);
     });
 
-    it.skip("should add books to the correct shelves automatically", function() {
+    it("should add books to the correct shelves automatically", function() {
+// denverLibrary object is assigned to the function createLibrary with the string name passed as an argument.
+//var dracula is an object with a key of genre and a string value identifying the correct shelf
       var denverLibrary = createLibrary("Denver Public Library");
       var dracula = {
         title: "Dracula",
@@ -71,14 +77,14 @@ describe("library.js", function() {
 
       addBook(denverLibrary, dracula);
       addBook(denverLibrary, paleBlueDot);
-
+//should return the object
       assert.equal(denverLibrary.shelves.fantasy[0], dracula);
       assert.equal(denverLibrary.shelves.nonFiction[0], paleBlueDot);
     });
   });
 
   describe("checkoutBook", function() {
-    it.skip("should unshelf a book to check out a book to a patron", function() {
+    it("should unshelf a book to check out a book to a patron", function() {
       var dracula = {
         title: "Dracula",
         mainCharacter: { name: "Count Dracula", age: undefined, pronouns: "he/him" },
@@ -102,7 +108,7 @@ describe("library.js", function() {
       addBook(denverLibrary, dracula);
       addBook(denverLibrary, bornACrime);
       addBook(denverLibrary, prideAndPrejudice);
-
+//checkoutBook needs to take library object, title and the genre
       var result1 = checkoutBook(denverLibrary, "Pride and Prejudice", "fiction");
 
       assert.deepEqual(denverLibrary.shelves, {fantasy: [dracula], fiction: [], nonFiction: [bornACrime]});;
@@ -114,7 +120,7 @@ describe("library.js", function() {
       assert.equal(result2, "You have now checked out Born a Crime from the Denver Public Library")
     });
 
-    it.skip("should only checkout a book if the book is on the shelves", function() {
+    it("should only checkout a book if the book is on the shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
 
       var error1 = checkoutBook(denverLibrary, "The Fifth Season", "fantasy");
